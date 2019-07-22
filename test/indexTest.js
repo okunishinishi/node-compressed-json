a7fb32397576f44c83118ca1a17f2238511a8818
okunishinishi <okunishinishi@gmail.com>
2019-07-22 23:52:11 +0900

    add compressor

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,6 +5,7 @@
 
 const index = require('../lib/index')
 const { ok, equal } = require('assert').strict
+const { describe, it } = require('mocha')
 
 describe('index', () => {
   it('compress/decompress', () => {
@@ -19,7 +20,7 @@ describe('index', () => {
       ddd: {
         eee: [
           [
-            ['Hey',12, null]
+            ['Hey', 12, null]
           ]
         ],
         fff: {
@@ -27,10 +28,10 @@ describe('index', () => {
         }
       },
       jjjj: void (0),
-      hhh:'hoge2',
-      nnn:'hoge',
-      asdf:'hoge',
-      jjj:'hoge',
+      hhh: 'hoge2',
+      nnn: 'hoge',
+      asdf: 'hoge',
+      jjj: 'hoge',
       n: 'p:143243',
       j: 'e:3'
     })
@@ -52,18 +53,46 @@ describe('index', () => {
     equal(restored.j, 'e:3')
   })
 
-  it('Shrink with large data', () => {
+  it('Handle array', () => {
+    const { compress, decompress } = index
+
+    const restored =
+      decompress(
+        compress([{ a: 1, b: 2 }, { c: 3 }])
+      )
+    ok(Array.isArray(restored), restored)
+  })
+
+  it('Large Array', () => {
     const { compress } = index
     const arr = new Array(10000).fill(null).map((_, i) => ({
       index: i,
       name: `This is item-${i}`,
-      text:'x#123143243234324324242t343234234',
-      text2:'x#123213',
+      text: 'x#123143243234324324242t343234234',
+      text2: 'x#123213',
       $$entity: true,
     }))
     ok(JSON.stringify(arr).length > compress.toString(arr).length)
     const rate = compress.toString(arr).length / JSON.stringify(arr).length
-    console.log('compress rage',rate)
+    console.log('\n[Large Array] compress rage', rate)
+  })
+
+  it('Large Object', () => {
+    const { compress } = index
+    const obj = Object.assign({},
+      ...new Array(10000).fill(null).map((_, i) => ({
+        [`k-${i}`]:{
+          index: i,
+          name: `This is item-${i}`,
+          text: 'x#123143243234324324242t343234234',
+          text2: 'x#123213',
+          $$entity: true,
+        }
+      }))
+    )
+    ok(JSON.stringify(obj).length > compress.toString(obj).length)
+    const rate = compress.toString(obj).length / JSON.stringify(obj).length
+    console.log('\n[Large Object] compress rage', rate)
   })
 })
 
